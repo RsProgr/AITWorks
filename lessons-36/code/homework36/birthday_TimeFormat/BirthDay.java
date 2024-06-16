31669e12fc3acb9f948f162402a00a79e4151357
Your Name <you@example.com>
2024-06-16 23:47:59 +0300

    added homework36_Birthday

--- lessons-36/code/homework36/birthday_TimeFormat/BirthDay.java
+++ lessons-36/code/homework36/birthday_TimeFormat/BirthDay.java
@@ -1,2 +1,24 @@
-package homework36.birthday_TimeFormat;public class BirthDay {
+package homework36.birthday_TimeFormat;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class BirthDay {
+    public static void main(String[] args) {
+        LocalDate birthDayStart = LocalDate.of(1989, 05, 11);
+        System.out.println(birthDayStart);
+        System.out.println("--------------------------");
+
+        DateTimeFormatter df = DateTimeFormatter.ISO_DATE;
+        System.out.println(birthDayStart.format(df));
+
+        System.out.println("--------------------------");
+        df = DateTimeFormatter.ofPattern("dd MMMM YYYY", Locale.FRANCE);
+        System.out.println(birthDayStart.format(df));
+
+        System.out.println("==============parsing date from string=================");
+        String str = "11 05 1989";
+        System.out.println(str);
+    }
 }
