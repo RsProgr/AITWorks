35820327bba1c6993b9428d39a095ad7bf584c14
Your Name <you@example.com>
2024-06-17 17:07:56 +0300

    BirthDay upd

--- lessons-36/code/homework36/birthday_TimeFormat/BirthDay.java
+++ lessons-36/code/homework36/birthday_TimeFormat/BirthDay.java
@@ -6,19 +6,39 @@ import java.util.Locale;
 
 public class BirthDay {
     public static void main(String[] args) {
-        LocalDate birthDayStart = LocalDate.of(1989, 05, 11);
-        System.out.println(birthDayStart);
-        System.out.println("--------------------------");
+        LocalDate Birthday = LocalDate.of(1989, 05, 11);
+        System.out.println(Birthday);
+        System.out.println("------------------------");
 
         DateTimeFormatter df = DateTimeFormatter.ISO_DATE;
-        System.out.println(birthDayStart.format(df));
+        System.out.println(Birthday.format(df));
+        System.out.println("--------------------------------");
 
+        df = DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ENGLISH);
+        System.out.println(Birthday.format(df));
+        System.out.println("---------------------------");
+
+        df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println(Birthday.format(df));
+        System.out.println("-------------------------");
+
+        df = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        System.out.println(Birthday.format(df));
         System.out.println("--------------------------");
-        df = DateTimeFormatter.ofPattern("dd MMMM YYYY", Locale.FRANCE);
-        System.out.println(birthDayStart.format(df));
 
-        System.out.println("==============parsing date from string=================");
+        System.out.println("=============parsing date from string===============");
         String str = "11 05 1989";
         System.out.println(str);
+
+        System.out.println("=====================================================");
+        LocalDate date = dateParse(str);
+        System.out.println(date + " " + date.getDayOfWeek());
+
+    }
+
+    // извлекает из строки дату в формате ISO по формату текстовому
+    private static LocalDate dateParse(String date) {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("[dd MM yyyy][yyyy-MM-dd][dd/MM/yyyy][dd.MM.yyyy][dd-MM-yyyy]");
+        return LocalDate.parse(date, df);
     }
 }
