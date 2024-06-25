dca02f861c5bae84c2acd0997de18eb43aca0df4
Your Name <you@example.com>
2024-06-25 07:51:31 +0300

    added alphabet

--- lessons-39/code/homework39/alphabet/Alphabets.java
+++ lessons-39/code/homework39/alphabet/Alphabets.java
@@ -1,2 +1,21 @@
-package homework39.alphabet;public class Alphabets {
+package homework39.alphabet;
+//Задача 3. Заполните структуру типа ArrayList символами латинского алфавита от A(65) до Z(...).
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Alphabets {
+
+    public static void main(String[] args) {
+        List<Character> alphabetLists = new ArrayList<>();
+
+        for (int i = 65; i <= 90; i++) {
+            char ch = (char) i;
+            alphabetLists.add(ch);
+        }
+        System.out.println("Алфавит от A до Z:");
+        for (char ch : alphabetLists) {
+            System.out.println(ch + " ");
+        }
+    }
 }
