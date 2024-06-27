29e274122be3c83dacc2ee0379174f62687cb998
Your Name <you@example.com>
2024-06-27 13:23:38 +0300

    Update Bank

--- lessons-40/code/homework40/bank/CurrencyApp.java
+++ lessons-40/code/homework40/bank/CurrencyApp.java
@@ -6,42 +6,41 @@ import java.util.Set;
 
 public class CurrencyApp {
 
-    Set<String> curr = new HashSet<>();
+    Set<String> currencies = new HashSet<>();
 
     public boolean addCurrency(String currency) {
 
-        if (curr.contains(currency)) {
+        if (currencies.contains(currency)) {
             return false;
         } else {
-            curr.add(currency);
+            currencies.add(currency);
             return true;
         }
     }
-
 
     public static void main(String[] args) {
         CurrencyApp app = new CurrencyApp();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите наименование валюты (или 'exit' для выхода):");
+        System.out.println("Enter the name of the currency (or 'exit' to quit):");
 
         while (true) {
-            System.out.print("Введите валюту: ");
+            System.out.print("Enter currency: ");
             String currency = scanner.nextLine();
 
             if (currency.equalsIgnoreCase("exit")) {
-                break; // Завершаем программу, если введено 'exit'
+                break;
             }
 
             boolean added = app.addCurrency(currency);
             if (added) {
-                System.out.println("Валюта " + currency + " успешно добавлена.");
+                System.out.println("Currency " + currency + " successfully added.");
             } else {
-                System.out.println("Валюта " + currency + " уже существует.");
+                System.out.println("Currency " + currency + " already exists.");
             }
         }
 
         scanner.close();
-        System.out.println("Программа завершена.");
+        System.out.println("Program terminated.");
     }
 }
